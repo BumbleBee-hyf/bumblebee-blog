61afe5062ed67123a485a1c1e01ae4eca108fbcf
huangyif <31237524+BumbleBee-hyf@users.noreply.github.com>
2021-09-08 11:29:19 +0800

    修改备案

--- src/layouts/LoginUser/LoginUser.js
+++ src/layouts/LoginUser/LoginUser.js
@@ -6,6 +6,7 @@ import React, {Component} from  'react';
 import './LoginUser.less'
 import LoginDom from '../../components/LoginDom'
 import RegisterDom from "../../components/RegisterDom";
+import {Footer} from "antd/es/layout/layout";
 export default class LoginUser extends Component {
     state = {
         userList: [],
@@ -36,11 +37,21 @@ export default class LoginUser extends Component {
             height: document.documentElement.clientHeight,
             width: document.documentElement.clientWidth
         }
+        const backgroundStyle = {
+            background: '#ffffff96'
+        }
         return (
-            <div className='LoginUser' style={heightStyle}>
-                <div className='logoStyle'>Bumblebee.zone</div>
-                {this.state.flag === 'login' ? <LoginDom userList = {this.state.userList} callback={this.callback} changeFlag={this.changeFlag}/> : <RegisterDom userList = {this.state.userList} callback={this.callback} changeFlag={this.changeFlag}/>}
-            </div>
+            <>
+                <div className='LoginUser' style={heightStyle}>
+                    <div className='logoStyle'>Bumblebee.zone</div>
+                    {this.state.flag === 'login' ? <LoginDom userList = {this.state.userList} callback={this.callback} changeFlag={this.changeFlag}/> : <RegisterDom userList = {this.state.userList} callback={this.callback} changeFlag={this.changeFlag}/>}
+                </div>
+                <Footer style={backgroundStyle}>
+                    <li>Designed & Powerd by bumblebee-hyf</li>
+                    <li> Copyright© 2021-2022 黄义峰-大黄蜂</li>
+                    <a href='https://beian.miit.gov.cn/#/Integrated/index'>粤ICP备2021075878号</a>
+                </Footer>
+            </>
         )
     }
 }
